18d7adc61058424933fbe801b42941678e3e420f
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-09-23 20:09:58 +0700

    Structure The App And Ready To GO

---
+++ src/app/Firebase/configFirebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyBP5IEcuOxJFjBODC68E4ArWF1b2aQUT_Q",
+    authDomain: "whats-app-clon.firebaseapp.com",
+    databaseURL: "https://whats-app-clon.firebaseio.com",
+    projectId: "whats-app-clon",
+    storageBucket: "whats-app-clon.appspot.com",
+    messagingSenderId: "553312226361",
+    appId: "1:553312226361:web:168b5f32ee06fd928551e8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig)
